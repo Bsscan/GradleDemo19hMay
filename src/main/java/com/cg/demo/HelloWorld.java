d30102b040abab3757c9253cb0e12d863636ffe7
bsscan <bhargavsaikia3@gmail.com>
2016-05-19 17:18:35 +0530

    Signed-off-by: bsscan <bhargavsaikia3@gmail.com>

---
+++ src/main/java/com/cg/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.cg.demo;
+
+public class HelloWorld{
+
+public static void main(String [] args){
+
+System.out.println("U HAVE REACHED GRADLE MAIN!!!!!!");
+
+}
+
+}
---
+++ src/test/java/tester/DemoTest.java
@@ -0,0 +1,20 @@
+package tester;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class DemoTest{
+
+@Test
+public void alwaysReturnPositiveResult(){
+
+assertTrue(true);
+
+}
+
+
+
+}
